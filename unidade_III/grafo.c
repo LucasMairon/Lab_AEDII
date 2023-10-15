ba330fb231cddb237df091f00662eb014389fa97
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-10-15 21:21:09 +0000

    alterações basicas

--- unidade_III/grafo.c
+++ unidade_III/grafo.c
@@ -8,18 +8,13 @@
 
 #define MAX_TAM_VERTICES 167
 
-// struct que representa um grafo, nela temos a variavel que guarda a quantidade
-// de vertices, a que guarda a quantidade de arestas e a matriz de adjacencia
 struct grafo {
   int vertices;
   int arestas;
   Vertice **g;
 };
 
-// struct que representa um vertice, temos a variavel que guarda o id do vertice
-// e o ponteiro para o proximo vertice
 struct vertice {
-  // int vertice; ??????
   int cep;
   char cityName[MAX_CHAR];
   Aresta *arestas;
@@ -30,7 +25,7 @@ struct aresta {
   Aresta *prox;
 };
 
-// cria a inicializa o grafo
+
 Grafo *cria_grafo() {
   Grafo *gr = (Grafo *)malloc(sizeof(Grafo));
   gr->g = (Vertice **)malloc(sizeof(Vertice *) * MAX_TAM_VERTICES);
